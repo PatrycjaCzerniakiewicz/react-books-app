fc570336c9594187adfc5094ce17e01802ed9e78
unknown <you@example.com>
2020-10-23 12:32:37 +0200

    Updates

---
+++ precache-manifest.1d373c0c1ed50a67f74ccf2a23c3d541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a588414548eaa96cd608df79ec184be",
+    "url": "/react-books-app/index.html"
+  },
+  {
+    "revision": "d3441830254c895018a5",
+    "url": "/react-books-app/static/css/main.a7dd46f0.chunk.css"
+  },
+  {
+    "revision": "5379e9df20f61ffcd825",
+    "url": "/react-books-app/static/js/2.b894f497.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-books-app/static/js/2.b894f497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3441830254c895018a5",
+    "url": "/react-books-app/static/js/main.ea5dd211.chunk.js"
+  },
+  {
+    "revision": "83a4c2c2767ba46333fb",
+    "url": "/react-books-app/static/js/runtime-main.2aa852fc.js"
+  }
+]);
